50afd5bfdf13967f2b50224db5a5ccbb59f44d4c
TesteMRP99 <mateus.paiva99@hotmail.com>
2025-03-17 17:39:23 -0300

    adicao dos movimentos torre, bispo e rainha

--- xadrezNivelNovato.c
+++ xadrezNivelNovato.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 	int main(){
-		int torre, bispo, rainha; //declaração de variavel para cada peça do xadrex
+		int torre, bispo, rainha; //declaração de variavel para cada peça do xadres
 		int jogada = 1; // variavel par determinar o numero de movimentos de cada peça
 		
 		printf("		Bem-Vindo ao Xadrez Virtual Estacio!\n");
